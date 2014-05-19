fac83e0601b4f8e815d36677c807d257102b7db9
bigua <bigua.kun@gmail.com>
2014-05-19 03:45:37 -0300

    Enviando um projeto basico

---
+++ Photobox/Photobox/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Photobox.Resources;
+
+namespace Photobox
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
